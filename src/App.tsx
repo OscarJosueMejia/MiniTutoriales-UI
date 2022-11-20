20a4d2425151e2f8db9ff62b7385ee2041b91624
mejiaSoc4 <mejiaoscar43@gmail.com>
2022-11-19 20:08:23 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+//import Home from '@views/Home';
+//import Cards from '@views/Cards';
+import TodoList from '@views/TodoList';
+function App() {
+  return (
+    <>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
